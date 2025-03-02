60952e9afce7527760d7d93e3dc369ff7f786436
VNABEANS <vnabeans@gmail.com>
2025-03-02 14:33:55 +1100

    Add files via upload

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.css$": "jest-css-modules-transform",
+  },
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["./jest.setup.tsx"],
+  preset: "ts-jest",
+  testMatch: ["<rootDir>/src/**/*.test.(ts|tsx)"],
+};
